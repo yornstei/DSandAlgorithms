ff2760e63f4542fc876fe0acbf57b501df91d3e3
Yehuda Ornstein <yornstein@fmm.com>
2019-10-29 18:14:26 -0400

    add bubble and merge sort

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 
 namespace Algorithms
 {
@@ -7,80 +9,110 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Stopwatch stopwatch = new Stopwatch();
-            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            //Console.WriteLine("Hello World!");
+            //Stopwatch stopwatch = new Stopwatch();
+            //string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
 
-            int count = Recursion.CountOfTxtFilesInDir(path);
-            Console.WriteLine($@"Txt file count in dir {path} = {count}");
+            //int count = Recursion.CountOfTxtFilesInDir(path);
+            //Console.WriteLine($@"Txt file count in dir {path} = {count}");
 
-            Recursion.PrintTxtFilesInDir(@"C:\Users\yornstein\Desktop");
+            //Recursion.PrintTxtFilesInDir(@"C:\Users\yornstein\Desktop");
 
-            stopwatch.Start();
-            Console.WriteLine($"Fib(5) = {Memoization.FibRecursion(5)}");
-            Console.WriteLine($"Fib(9) = {Memoization.FibRecursion(9)}");
-            Console.WriteLine($"Fib(15) = {Memoization.FibRecursion(15)}");
-            Console.WriteLine($"Fib(35) = {Memoization.FibRecursion(35)}");
-            stopwatch.Stop();
-            Console.WriteLine(stopwatch.Elapsed);
+            //stopwatch.Start();
+            //Console.WriteLine($"Fib(5) = {Memoization.FibRecursion(5)}");
+            //Console.WriteLine($"Fib(9) = {Memoization.FibRecursion(9)}");
+            //Console.WriteLine($"Fib(15) = {Memoization.FibRecursion(15)}");
+            //Console.WriteLine($"Fib(35) = {Memoization.FibRecursion(35)}");
+            //stopwatch.Stop();
+            //Console.WriteLine(stopwatch.Elapsed);
 
 
-            stopwatch.Reset();
-            stopwatch.Start();
-            Console.WriteLine($"Fib(5) = {Memoization.FibMemoized(5)}");
-            Console.WriteLine($"Fib(9) = {Memoization.FibMemoized(9)}");
-            Console.WriteLine($"Fib(15) = {Memoization.FibMemoized(15)}");
-            Console.WriteLine($"Fib(35) = {Memoization.FibMemoized(35)}");
-            stopwatch.Stop();
-            Console.WriteLine(stopwatch.Elapsed);
+            //stopwatch.Reset();
+            //stopwatch.Start();
+            //Console.WriteLine($"Fib(5) = {Memoization.FibMemoized(5)}");
+            //Console.WriteLine($"Fib(9) = {Memoization.FibMemoized(9)}");
+            //Console.WriteLine($"Fib(15) = {Memoization.FibMemoized(15)}");
+            //Console.WriteLine($"Fib(35) = {Memoization.FibMemoized(35)}");
+            //stopwatch.Stop();
+            //Console.WriteLine(stopwatch.Elapsed);
 
-            int[,] grid = new int[,] 
-            {
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 1, 0, 0, 0, 1, 0},
-                {0, 0, 0, 0, 1, 0, 0, 0},
-                {1, 0, 1, 0, 0, 1, 0, 0},
-                {0, 0, 1, 0, 0, 0, 0, 0},
-                {0, 0, 0, 1, 1, 0, 1, 0},
-                {0, 1, 0, 0, 0, 1, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-            };
+            //int[,] grid = new int[,] 
+            //{
+            //    {0, 0, 0, 0, 0, 0, 0, 0},
+            //    {0, 0, 1, 0, 0, 0, 1, 0},
+            //    {0, 0, 0, 0, 1, 0, 0, 0},
+            //    {1, 0, 1, 0, 0, 1, 0, 0},
+            //    {0, 0, 1, 0, 0, 0, 0, 0},
+            //    {0, 0, 0, 1, 1, 0, 1, 0},
+            //    {0, 1, 0, 0, 0, 1, 0, 0},
+            //    {0, 0, 0, 0, 0, 0, 0, 0},
+            //};
 
-            Console.WriteLine($"PathsCount Recursion {DynamicProgramming.PathsCountRecursion(grid, 0, 0)}");
-            Console.WriteLine($"PathsCount Memoized {DynamicProgramming.PathsCountMemoized(grid, 0, 0)}");
-            Console.WriteLine($"PathsCount DynamicProg {DynamicProgramming.PathsCountDynamicProg(grid, 0, 0)}");
+            //Console.WriteLine($"PathsCount Recursion {DynamicProgramming.PathsCountRecursion(grid, 0, 0)}");
+            //Console.WriteLine($"PathsCount Memoized {DynamicProgramming.PathsCountMemoized(grid, 0, 0)}");
+            //Console.WriteLine($"PathsCount DynamicProg {DynamicProgramming.PathsCountDynamicProg(grid, 0, 0)}");
 
-            Console.ReadKey();
+            //Console.ReadKey();
 
-            int[] nums = new int[] { 2, 3, 4, 5, 6, 7, 8, 10, 11, 14, 16 };
-            int[] nums2 = new int[] { 1, 3, 4, 5, 7, 8, 10, 12, 15, 18, 21, 22, 25 };
+            //int[] nums = new int[] { 2, 3, 4, 5, 6, 7, 8, 10, 11, 14, 16 };
+            //int[] nums2 = new int[] { 1, 3, 4, 5, 7, 8, 10, 12, 15, 18, 21, 22, 25 };
 
-            Console.WriteLine(Search.BinarySearchRecursion(nums, 9));
-            Console.WriteLine(Search.BinarySearchRecursion(nums2, 5));
-            Console.WriteLine(Search.BinarySearchRecursion(nums, 9, 0, nums.Length - 1));
-            Console.WriteLine(Search.BinarySearchRecursion(nums2, 5, 0, nums2.Length - 1));
-            Console.WriteLine(Search.BinarySearchLoop(nums, 9));
-            Console.WriteLine(Search.BinarySearchLoop(nums2, 5));
+            //Console.WriteLine(Search.BinarySearchRecursion(nums, 9));
+            //Console.WriteLine(Search.BinarySearchRecursion(nums2, 5));
+            //Console.WriteLine(Search.BinarySearchRecursion(nums, 9, 0, nums.Length - 1));
+            //Console.WriteLine(Search.BinarySearchRecursion(nums2, 5, 0, nums2.Length - 1));
+            //Console.WriteLine(Search.BinarySearchLoop(nums, 9));
+            //Console.WriteLine(Search.BinarySearchLoop(nums2, 5));
 
-            Random random = new Random();
-            int[] numbers = new int[1000];
-            for (int i = 0; i < 999; i++)
-            {
-                int rand = random.Next(1, 10000);
-                if (rand != 455)
-                    numbers[i] = rand;
-            }
+            //Random random = new Random();
+            //int[] numbers = new int[1000];
+            //for (int i = 0; i < 999; i++)
+            //{
+            //    int rand = random.Next(1, 10000);
+            //    if (rand != 455)
+            //        numbers[i] = rand;
+            //}
 
-            int value = numbers[501];
+            //int value = numbers[501];
 
-            Array.Sort(numbers);
+            //Array.Sort(numbers);
 
-            Console.WriteLine(Search.BinarySearchRecursion(numbers, value));
-            Console.WriteLine(Search.BinarySearchRecursion(numbers, 455));
-            Console.WriteLine(Search.BinarySearchRecursion(numbers, value, 0, numbers.Length - 1));
-            Console.WriteLine(Search.BinarySearchRecursion(numbers, 455, 0, numbers.Length - 1));
-            Console.WriteLine(Search.BinarySearchLoop(numbers, value));
-            Console.WriteLine(Search.BinarySearchLoop(numbers, 455));
+            //Console.WriteLine(Search.BinarySearchRecursion(numbers, value));
+            //Console.WriteLine(Search.BinarySearchRecursion(numbers, 455));
+            //Console.WriteLine(Search.BinarySearchRecursion(numbers, value, 0, numbers.Length - 1));
+            //Console.WriteLine(Search.BinarySearchRecursion(numbers, 455, 0, numbers.Length - 1));
+            //Console.WriteLine(Search.BinarySearchLoop(numbers, value));
+            //Console.WriteLine(Search.BinarySearchLoop(numbers, 455));
+
+            //Console.ReadKey();
+
+            int[] arr = new int[] { 4, 6, 3, 2, 7, 1, 5, 9, 8 };
+            arr.ToList().ForEach(x => Console.Write(x));
+
+            Console.WriteLine();
+
+            Sort.BubbleSort(arr);
+            arr.ToList().ForEach(x => Console.Write(x));
+
+            Console.WriteLine("\n---------");
+
+            arr = new int[] { 4, 6, 3, 2, 7, 1, 5, 9, 8, 10 };
+            arr.ToList().ForEach(x => Console.Write(x));
+
+            Console.WriteLine();
+
+            Sort.MergeSort(arr);
+            arr.ToList().ForEach(x => Console.Write(x));
+
+            Console.WriteLine("\n---------");
+
+            arr = new int[] { 4, 6, 3, 2, 7, 1, 5, 9, 8, 10 };
+            arr.ToList().ForEach(x => Console.Write(x));
+
+            Console.WriteLine();
+
+            Sort.QuickSort(arr);
+            arr.ToList().ForEach(x => Console.Write(x));
         }
     }
 }
---
+++ Algorithms/Sort.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    class Sort
+    {
+        //ckeck out https://www.youtube.com/watch?v=KF2j-9iSf4Q for explanation and code source
+        public static void BubbleSort(int[] arr)
+        {
+            bool sorted = false;
+            int sortedIndex = arr.Length;
+            while (!sorted)
+            {
+                sorted = true;
+                for (int i = 0; i < sortedIndex - 1; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        Swap(arr, i, i + 1);
+                        sorted = false;
+                    }
+                }
+                sortedIndex--;
+            }
+        }
+
+        public static void MergeSort(int[] arr)
+        {
+            MergeSortRecursion(arr, 0, arr.Length - 1, new int[arr.Length]);
+        }
+
+        private static void MergeSortRecursion(int[] arr, int start, int end, int[] temp)
+        {
+            if (start >= end) return;
+
+            int middle = (start + end) / 2;
+            MergeSortRecursion(arr, start, middle, temp);
+            MergeSortRecursion(arr, middle + 1, end, temp);
+            MergeSortedSides(arr, start, end, temp);
+        }
+
+        private static void MergeSortedSides(int[] arr, int leftStart, int rightEnd, int[] temp)
+        {
+            int leftEnd = (leftStart + rightEnd) / 2;
+            int rightStart = leftEnd + 1;
+
+            int right = rightStart;
+            int left = leftStart;
+            int index = leftStart;
+            int size = rightEnd - leftStart + 1;
+
+            while(left <= leftEnd && right <= rightEnd)
+            {
+                if (arr[left] <= arr[right])
+                {
+                    temp[index] = arr[left];
+                    left++;
+                }
+                else
+                {
+                    temp[index] = arr[right];
+                    right++;
+                }
+
+                index++;
+            }
+
+            Array.Copy(arr, left, temp, index, leftEnd - left + 1);
+            Array.Copy(arr, right, temp, index, rightEnd - right + 1);
+            Array.Copy(temp, leftStart, arr, leftStart, size);
+        }
+
+        private static void Swap(int[] arr, int i, int j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        public static void QuickSort(int[] arr) { }
+    }
+}
